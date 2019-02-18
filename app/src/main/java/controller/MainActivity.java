a14c4dbb516fb8d4716fd81872f7a1ee9f604c44
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-18 10:35:39 +0100

    user name attribut is now defined by mNameInput in launch of appl

--- app/src/main/java/controller/MainActivity.java
+++ app/src/main/java/controller/MainActivity.java
@@ -19,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
    private TextView mGreetingText;   // Déclaration des variables
    private EditText mNameInput;
    private Button mPlayButton;
-    User user;
-
-    User user1 = new User();
+   private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUser = new User();
 
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);   // Association des variables aux Vues graphiques
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
@@ -57,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 // The user just clicked
                 Intent gameActivity = new Intent (MainActivity.this, GameActivity.class);
                 startActivity(gameActivity);  // Quand on clique, switch d'activités.
-                user.setFirstName(mNameInput.getText().toString());  // Set le name de l'user en fonction de ce qui est rentré par l'utilisateur
+                String firstname =mNameInput.getText().toString();
+                mUser.setFirstName(firstname);  // Set le name de l'user en fonction de ce qui est rentré par l'utilisateur
             }
         });
     }
