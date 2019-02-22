922239594a706b0aca93c42f790df29ccbe17e72
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-22 10:31:45 +0100

    restructed methods order

--- app/src/main/java/controller/GameActivity.java
+++ app/src/main/java/controller/GameActivity.java
@@ -30,6 +30,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
      private int mPlayerScore;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,9 +59,45 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         mNumberOfQuestions = 15;
 
-        mCurrentQuestion =mQuestionBank.getQuestion();
+        mCurrentQuestion = mQuestionBank.getQuestion();
         this.displayQuestion(mCurrentQuestion);
+    }
 
+    private QuestionBank generateQuestions() {
+
+        Question question1 = new Question("Quel est le plus gros animal vivant sur Terre ?",
+                Arrays.asList("L'éléphant", "Le Calamar Géant", "La baleine", "Le requin baleine"),2);
+        Question question2 = new Question("Quelle est la capitale de l'Australie ?",
+                Arrays.asList("Sydney", "Melbourne", "Canberra", "Nyah"),2);
+        Question question3 = new Question("Combien y'a-t-il d'étoiles sur le drapeau Américain ?",
+                Arrays.asList("49","50","51", "Ca dépend"), 1);
+        Question question4 = new Question("Quelle est la lanque principalement parlée en Uruguay ?",
+                Arrays.asList("Espagnol", "Anglais", "Portugais", "Uruguayien"), 0);
+        Question question5 = new Question ("Quel est le pays oû le taux de bonheur était le plus élevé dans le monde en 2017?",
+                Arrays.asList("Danemark", "France", "Etats-Unis", "Japon"), 0);
+        Question question6 = new Question ("Quelle place occupe la France dans le classement des pays les plus heureux en 2018 ?",
+                Arrays.asList("5ème", "3ème", "9ème", "23ème"), 3);
+        Question question7 = new Question("Combien gagne BAYER pharmacologie chaque seconde qui passe ?",
+                Arrays.asList("1€", "6€", "15€", "44€"), 3);
+        Question question8 = new Question ("Combien d'animaux sont tués chaque seconde pour nourrir l'homme à travers le monde ?",
+                Arrays.asList("10", "70","300","2200"), 3);
+        Question question9 = new Question ("Quel nom porte le siège de la police londonienne ?",
+                Arrays.asList("Buckingham", "Scotland Yard", "Camden Town", "Notting Hill"), 1);
+        Question question10 = new Question ("Quel est l'autre nom de l'étude des fossiles:",
+                Arrays.asList("La Paléontologie", "La Geologie", "La Spéléologie","L'archéologie"), 0);
+        Question question11 = new Question ("Quelle planète du système solaire a une densité inférieure à l'eau ?",
+                Arrays.asList("Saturne","Jupiter", "Uranus","Pluton"),0);
+        Question question12 = new Question("Combien représente le montant de la fraude fiscale en 2018 ?",
+                Arrays.asList("15Mds €", "45Mds €", "70Mds €","100Mds €"), 3);
+        Question question13= new Question ("Quel pays est le 3ème plus gros vendeur d'armes au monde ?",
+                Arrays.asList("La Chine", "La France", "La Russie", "Les Etats-Unis"), 1);
+        Question question14 = new Question ("Quel Pays a vendu des armes à la Syrie entre 2005 et 2009 ?",
+                Arrays.asList("L'Arabie Saoudite","La Turquie", "La France","La Russie"),2);
+        Question question15 = new Question("Combien d'années faudrait-il travailler au Smic pour économiser le salaire de Jeff Bezos (Amazon) sur l'année 2018?",
+                Arrays.asList("100 ans", "500 ans", "1500 ans", "5 635 000 ans"),3);
+
+        return new QuestionBank(Arrays.asList(question1,question2,question3,question4,
+                question5,question6,question7,question8,question9,question10,question11,question12, question13, question14,question15));
 
     }
 
@@ -144,40 +181,4 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         mButton4.setText((CharSequence) question.getChoiceList().get(3));
     }
 
-    private QuestionBank generateQuestions() {
-        Question question1 = new Question("Quel est le plus gros animal vivant sur Terre ?",
-                Arrays.asList("L'éléphant", "Le Calamar Géant", "La baleine", "Le requin baleine"),2);
-        Question question2 = new Question("Quelle est la capitale de l'Australie ?",
-                Arrays.asList("Sydney", "Melbourne", "Canberra", "Nyah"),2);
-        Question question3 = new Question("Combien y'a-t-il d'étoiles sur le drapeau Américain ?",
-                Arrays.asList("49","50","51", "Ca dépend"), 1);
-        Question question4 = new Question("Quelle est la lanque principalement parlée en Uruguay ?",
-                Arrays.asList("Espagnol", "Anglais", "Portugais", "Uruguayien"), 0);
-        Question question5 = new Question ("Quel est le pays oû le taux de bonheur était le plus élevé dans le monde en 2017?",
-                Arrays.asList("Danemark", "France", "Etats-Unis", "Japon"), 0);
-        Question question6 = new Question ("Quelle place occupe la France dans le classement des pays les plus heureux en 2018 ?",
-                Arrays.asList("5ème", "3ème", "9ème", "23ème"), 3);
-        Question question7 = new Question("Combien gagne BAYER pharmacologie chaque seconde qui passe ?",
-                Arrays.asList("1€", "6€", "15€", "44€"), 3);
-        Question question8 = new Question ("Combien d'animaux sont tués chaque seconde pour nourrir l'homme à travers le monde ?",
-                Arrays.asList("10", "70","300","2200"), 3);
-        Question question9 = new Question ("Quel nom porte le siège de la police londonienne ?",
-                Arrays.asList("Buckingham", "Scotland Yard", "Camden Town", "Notting Hill"), 1);
-        Question question10 = new Question ("Quel est l'autre nom de l'étude des fossiles:",
-                Arrays.asList("La Paléontologie", "La Geologie", "La Spéléologie","L'archéologie"), 0);
-        Question question11 = new Question ("Quelle planète du système solaire a une densité inférieure à l'eau ?",
-                Arrays.asList("Saturne","Jupiter", "Uranus","Pluton"),0);
-        Question question12 = new Question("Combien représente le montant de la fraude fiscale en 2018 ?",
-                Arrays.asList("15Mds €", "45Mds €", "70Mds €","100Mds €"), 3);
-        Question question13= new Question ("Quel pays est le 3ème plus gros vendeur d'armes au monde ?",
-                Arrays.asList("La Chine", "La France", "La Russie", "Les Etats-Unis"), 1);
-        Question question14 = new Question ("Quel Pays a vendu des armes à la Syrie entre 2005 et 2009 ?",
-                Arrays.asList("L'Arabie Saoudite","La Turquie", "La France","La Russie"),2);
-        Question question15 = new Question("Combien d'années faudrait-il travailler au Smic pour économiser le salaire de Jeff Bezos (Amazon) sur l'année 2018?",
-                Arrays.asList("100 ans", "500 ans", "1500 ans", "5 635 000 ans"),3);
-
-        return new QuestionBank(Arrays.asList(question1,question2,question3,question4,
-                question5,question6,question7,question8,question9,question10,question11,question12, question13, question14,question15));
-
-    }
 }
