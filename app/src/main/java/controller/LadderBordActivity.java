53a7ca4ddd1a504528d1d69df77ebe022e85facd
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-25 14:27:18 +0100

    created ladderbord activity + layout

---
+++ app/src/main/java/controller/LadderBordActivity.java
@@ -0,0 +1,15 @@
+package controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.matt.android.topquiz.R;
+
+public class LadderBordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ladder_bord);
+    }
+}
