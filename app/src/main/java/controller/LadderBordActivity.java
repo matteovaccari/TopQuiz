bd59e14ba95f1f15eac2f803064b13c618cc9a55
Matteoo VACCARI <matteo.vaccari@yahoo.com>
2019-02-27 17:03:40 +0100

    created ladderbordActivity

--- app/src/main/java/controller/LadderBordActivity.java
+++ app/src/main/java/controller/LadderBordActivity.java
@@ -1,28 +1,50 @@
 package controller;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 import com.matt.android.topquiz.R;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import com.google.gson.Gson;
+
 public class LadderBordActivity extends AppCompatActivity {
-    private TextView player1Score;
-    private TextView player2Score;
-    private TextView player3Score;
-    private TextView player4Score;
-    private TextView player5Score;
+    private TextView player1;
+    private TextView player2;
+    private TextView player3;
+    private TextView player4;
+    private TextView player5;
+    private List<TextView> playerList;
+    private SharedPreferences mPreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ladder_bord);
-        player1Score = (TextView) findViewById(R.id.ladderbord_player1_id);
-        player2Score = (TextView) findViewById(R.id.ladderbord_player2_id);
-        player3Score = (TextView) findViewById(R.id.ladderbord_player3_id);
-        player4Score = (TextView) findViewById(R.id.ladderbord_player4_id);
-        player5Score = (TextView) findViewById(R.id.ladderbord_player5_id);
+
+        player1 = (TextView) findViewById(R.id.ladderbord_player1_id);
+        player2 = (TextView) findViewById(R.id.ladderbord_player2_id);
+        player3 = (TextView) findViewById(R.id.ladderbord_player3_id);
+        player4 = (TextView) findViewById(R.id.ladderbord_player4_id);
+        player5 = (TextView) findViewById(R.id.ladderbord_player5_id);
+
+        for (int i = 0; i <= 4; i++) {
+            mPreferences.getString()
+        }
 
     }
+
 }
